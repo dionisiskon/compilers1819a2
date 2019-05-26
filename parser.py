8445cb31b1d768ed88f2c5858b0f15e46262f9c1
Διονύσιος Κοντοζίδης <32637139+dionisiskon@users.noreply.github.com>
2019-05-26 12:40:55 +0300

    Διόρθωση Rep στα bits με Rep1 για 1 ή περισσότερες φορές

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+
 import plex
 
 class ParseError(Exception):
@@ -11,8 +12,9 @@ class MyParser:
 		digit = plex.Range('09')
 		name = letter+plex.Rep(letter|digit)
 		bit = plex.Range('01')
-		bits = bit + plex.Rep(bit)
+		bits = plex.Rep1(bit)
 		keyword = plex.Str('print','PRINT')
+		space = plex.Any(" \n\t")
 		operator=plex.Str('=','^','&','|')
 		self.lexicon = plex.Lexicon([
 			(operator,plex.TEXT),
@@ -31,7 +33,7 @@ class MyParser:
 		return self.scanner.read()
 
 	def match(self,token):
-		if self.la==token:
+		if self.la == token:
 			self.la,self.text=self.next_token()
 		else:
 			raise ParseError("perimenw (")
@@ -41,19 +43,19 @@ class MyParser:
 		self.stmt_list()
 		
 	def stmt_list(self):
-		if self.la=='IDENTIFIER' or self.la=='PRINT':
+		if self.la == 'IDENTIFIER' or self.la == 'PRINT':
 			self.stmt()
 			self.stmt_list()
-		elif self.la==None:
+		elif self.la == None:
 			return
 		else:
 			raise ParseError("perimenw IDENTIFIER or Print")
 	def stmt(self):
-		if self.la=='IDENTIFIER':
+		if self.la == 'IDENTIFIER':
 			self.match('IDENTIFIER')	
 			self.match('=')
 			self.expr()
-		elif self.la=='PRINT':
+		elif self.la == 'PRINT':
 			self.match('PRINT')
 			self.expr()
 		else:
@@ -65,31 +67,31 @@ class MyParser:
 		else:
 			raise ParseError("perimenw ( or IDENTIFIER or BIT_TOKEN or )")
 	def term_tail(self):	
-		if self.la=='^':
+		if self.la == '^':
 			self.match('^')
 			self.term()
 			self.term_tail()
-		elif self.la=='IDENTIFIER' or self.la=='PRINT' or self.la== None or self.la==')':
+		elif self.la == 'IDENTIFIER' or self.la == 'PRINT' or self.la == None or self.la == ')':
 			return
 		else:
 			raise ParseError("perimenw ^")
 	def term(self):
-		if self.la=='(' or self.la=='IDENTIFIER' or self.la==')' or self.la == 'BIT_TOKEN':	
+		if self.la == '(' or self.la == 'IDENTIFIER' or self.la == 'BIT_TOKEN':	
 			self.factor()
 			self.factor_tail()
 		else:
 			raise ParseError("perimenw ( or IDENTIFIER or )")
 	def factor_tail(self):
-		if self.la=='|':
+		if self.la == '|':
 			self.match('|')
 			self.factor()
 			self.factor_tail()
-		elif self.la=='^' or self.la=='IDENTIFIER' or self.la=='PRINT' or self.la== None or self.la==')':
+		elif self.la == '^' or self.la == 'IDENTIFIER' or self.la == 'PRINT' or self.la == None or self.la == ')':
 			return
 		else:
 			raise ParseError("perimenw |")
 	def factor(self):
-		if self.la=='(' or self.la == 'IDENTIFIER' or self.la == 'BIT_TOKEN':
+		if self.la == '(' or self.la == 'IDENTIFIER' or self.la == 'BIT_TOKEN':
 			self.atom()
 			self.atom_tail()
 		else:
@@ -99,21 +101,22 @@ class MyParser:
 			self.match('&')
 			self.atom()
 			self.atom_tail()
-		elif self.la=='|' or self.la=='^' or self.la=='IDENTIFIER' or self.la=='PRINT' or self.la== None or self.la==')':
+		elif self.la == '|' or self.la == '^' or self.la == 'IDENTIFIER' or self.la == 'PRINT' or self.la == None or self.la == ')':
 			return
 		else:
 			raise ParseError("perimenw &")
 	def atom(self):
-		if self.la=='(':
+		if self.la == '(':
 			self.match('(')
 			self.expr()
 			self.match(')')
-		elif self.la=='IDENTIFIER':
+		elif self.la == 'IDENTIFIER':
 			self.match('IDENTIFIER')
-		elif self.la=='BIT_TOKEN':
+		elif self.la == 'BIT_TOKEN':
 			self.match('BIT_TOKEN')
 		else:
 			raise ParseError("perimenw id bit or (")
+
 parser = MyParser()
 with open('test.txt','r') as fp:
 	parser.parse(fp)
